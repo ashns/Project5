d3bb382bc8797be6069538d2b2138389d08bf61d
mbw84 <mbw84@scarletmail.rutgers.edu>
2021-04-16 17:38:27 -0400

    donut button implemented?
    Pushing it will make the app crash

--- app/src/main/java/com/example/project5/MainActivity.java
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -15,17 +15,19 @@ import android.widget.Button;
 
         public Button donutBTN;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            }
 
-    public void showDonut(View view) {
-        setContentView(R.layout.activity_donuts);
-        Intent intent = new Intent(this, DonutActivity.class);
 
-       // intent.putExtra("ORDER", currentOrder);
-        startActivity(intent);
-        }
+        public void showDonut(View view) {
+            setContentView(R.layout.activity_donuts);
+            Intent intent = new Intent(this, DonutActivity.class);
+
+            // intent.putExtra("ORDER", currentOrder);
+            startActivity(intent);
+            }
+
 }

882c8a9a4fac0fd8876c0418c8d9cb814faa3ef5
ashns <35615496+ashns@users.noreply.github.com>
2021-04-23 21:18:52 -0400

    commented StoreOrderActivity

--- app/src/main/java/com/example/project5/StoreOrderActivity.java
+++ app/src/main/java/com/example/project5/StoreOrderActivity.java
@@ -10,16 +10,17 @@ import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.widget.Toast;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.CheckBox;
 import androidx.appcompat.app.AlertDialog;
-
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The controller class responds to all input from the store order UI with appropriate
+ * calls to methods in other classes.
+ * Provides client methods: onCreate, onDestroy, returnToMain
+ * @author Ashley Stankovits, Matthew Walker
+ */
 public class StoreOrderActivity extends Activity {
 
     Order currentOrder;
@@ -33,6 +34,11 @@ public class StoreOrderActivity extends Activity {
     DecimalFormat usd = new DecimalFormat("#.##");
 
 
+    /**
+     * This method initializes all views and listeners on this
+     * screen.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +58,15 @@ public class StoreOrderActivity extends Activity {
 
         orderLV.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
+            /**
+             * This is a listener for when the store order ListView is clicked.
+             * An alert box then pops up asking to confirm the deletion
+             * of an item from the listview.
+             * @param parent which is the parent AdapterView
+             * @param view the open screen
+             * @param position which is the location in the list that is clicked
+             * @param id of the listview clicked item
+             */
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 index = position;
@@ -60,6 +75,13 @@ public class StoreOrderActivity extends Activity {
                 alertDialog.setMessage("Would you like to delete this item?");
                 alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
 
+                    /**
+                     * This is a listener for the "yes" button on the
+                     * dialog box which confirms deletion of an item
+                     * from the listview.
+                     * @param dialog which is the dialog box clicked
+                     * @param which which is the selected answer.
+                     */
                     public void onClick(DialogInterface dialog, int which) {
                         try {
                             currentOrder.remove(index);
@@ -80,6 +102,11 @@ public class StoreOrderActivity extends Activity {
 
                 alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
 
+                    /**
+                     * This is a listener for the "no" option on the dialog box
+                     * @param dialog which is the corresponding dialog box
+                     * @param which which is the answer selected
+                     */
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
@@ -95,11 +122,20 @@ public class StoreOrderActivity extends Activity {
         });
     }
 
+    /**
+     * This method defines a basic on destroy action.
+     */
     @Override
     protected void onDestroy(){
         super.onDestroy();
     }
 
+    /**
+     * This method responds to the corresponding button
+     * being clicked by finishing what's done on this screen
+     * and switching back to the main menu screen.
+     * @param view which is the return button being clicked.
+     */
     public void returnToMain(View view){
         finish();
     }
